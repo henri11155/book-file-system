e00de55c299e68d35307950ba926962f534f5221
henri111555 <henriblob123@gmail.com>
2026-08-25 17:14:57 +1000

    shift forward added
    can shift elements back to remove empty spaces

--- main.cpp
+++ main.cpp
@@ -5,21 +5,28 @@
 // a program that tracks items with add/remove/search
 // store books as as struct
 
+using maxSize = uint16_t;
+
 struct book {
     std::string name {};
     std::string author {};
-    uint16_t id {};
+    maxSize id {};
     book() = default;
-    book(std::string name, std::string author, uint16_t id) : name{name}, author{author}, id{id} {}
+    book(std::string name, std::string author, maxSize id) : name{name}, author{author}, id{id} {}
 };
 
 class fileSystem {
+
     private:
     book* books { nullptr };
-    uint32_t capacity{};
-    uint32_t count{};
+    maxSize capacity{};
+    maxSize count{};
     public:
-    fileSystem(size_t count=8) : books{new book[capacity]}, capacity{static_cast<uint32_t>(count)}, count{0} {}
+    enum direction {
+        forwards = 1,
+        backwards = 0
+    };
+    fileSystem(size_t count=8) : books{new book[capacity]}, capacity{static_cast<maxSize>(count)}, count{0} {}
 
     //fileSystem(fileSystem&& fileSystem) noexcept : books{fileSystem.books}, capacity{fileSystem.capacity}, count{fileSystem.count}
     //{
@@ -45,16 +52,39 @@ class fileSystem {
         count++;
     }
     
-    void remove(uint16_t id) {}
+    void remove(maxSize id) {}
     
+    void shift (direction dir, size_t start) noexcept {
+        size_t fast {start+1};
+        size_t slow {start};
+        if (dir == forwards) {
+            while (fast < capacity) {
+                if (!books[fast].id) {
+                    continue;
+                } else if (!books[slow].id) {
+                    std::cout << " moving " << books[fast].id << " to " << books[slow].id;
+                    books[slow] = std::move(books[fast]);
+                    slow++;
+                    break;
+                }
+                fast++;
+            }
+        }
+        else {
+
+            
+        }
+    }
     //book search(uint16_t id) {}
     
     void save() {}
 
     void printBooks() {
-        std::cout << books[0].name;
-        std::cout << books[1].name;
-        std::cout << books[2].name;
+        std::cout << "pritning";
+        for (size_t i {0}; i < capacity; i++) {
+            if (books[i].id){
+            std::cout << books[i].id << books[i].name;
+        }}
         }
     };
 
@@ -62,13 +92,16 @@ class fileSystem {
 
 
 int main() {
-    fileSystem test1 {fileSystem(0)};
+    fileSystem test1 {fileSystem(8)};
     book harrypotter("harry", "jk", 12);
-    book harrypotter2("harry2", "jk", 12);
+    book harrypotter2("harry2", "jk", 0);
     book harrypotter3("harry3", "jk", 12);
+    
     test1.add(harrypotter);
     test1.add(harrypotter2);
     test1.add(harrypotter3);
+    test1.printBooks();
+    test1.shift(fileSystem::direction::forwards, 1);
     test1.printBooks();
     //test1.resize();
 }
